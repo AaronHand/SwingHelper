16e81080888a7246be250fe22b1a0848bcb214bf
StefanoGitHub <stefano.borghi@gmail.com>
2015-07-29 18:23:40 -0700

    Adds Swing and Swing_DB classes.
    The Swing object contains the date of the swing, the name of the player, and the fileName of the recorded video.
    The database contains only a single "t_swings" table

---
+++ app/src/main/java/com/group2/swinghelper/Swing.java
@@ -0,0 +1,79 @@
+package com.group2.swinghelper;
+
+import android.annotation.SuppressLint;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Stefano on 7/29/15.
+ */
+public class Swing {
+
+    private long id;
+    private long dateMillis;
+    private String player;
+    private String fileName;
+
+
+    //constructors
+    public Swing() {
+        setId(0);
+        setDateMillis(System.currentTimeMillis());
+        setPlayer("");
+        setFileName("");
+    }
+
+
+    public Swing(long dateMillis, String player, String fileName) {
+        //this.setId(id);
+        this.setDateMillis(dateMillis);
+        this.setPlayer(player);
+        this.setFileName(fileName);
+    }
+
+    public Swing(long id, long dateMillis, String player, String fileName) {
+        this.setId(id);
+        this.setDateMillis(dateMillis);
+        this.setPlayer(player);
+        this.setFileName(fileName);
+    }
+
+
+    //getters
+    public long getId() {
+        return id;
+    }
+    public long getDateMillis() {
+        return dateMillis;
+    }
+    @SuppressLint("SimpleDateFormat")
+    public String getDateStringFormatted() {
+        // set the date with formatting
+        Date date = new Date(dateMillis);
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM d yyyy HH:mm:ss");
+        return sdf.format(date);
+    }
+    public String getPlayer() { return player; }
+    public String getFileName() { return fileName; }
+
+
+    //setters
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setDateMillis(long dateMillis) {
+        this.dateMillis = dateMillis;
+    }
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+    //toString
+    public String toString() { return getDateStringFormatted(); }
+
+
+}
